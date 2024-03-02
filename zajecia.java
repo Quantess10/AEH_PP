56a2a604ba106ed1a2ebcc7bae627e5306a358a7
Quantess10 <lukaszolesik@gmail.com>
2024-03-02 16:24:00 +0100

    zadanka

--- zajecia.java
+++ zajecia.java
@@ -5,6 +5,7 @@ class excercises {
         exercise3();
         exercise4();
         exercise5();
+        exercise6();
     }
 
     // 1) Wyświetl wynik dodawania, odejmowania, mnożenia oraz dzielenia 2 wartości.
@@ -17,10 +18,10 @@ class excercises {
         int iloczyn = x * y;
         double iloraz = x / y;
 
-        System.out.println("Suma: " + suma);
-        System.out.println("Różnica: " + roznica);
-        System.out.println("Iloczyn: " + iloczyn);
-        System.out.println("Iloraz: " + iloraz);
+        System.out.println(x + " + " + y + " = " + suma);
+        System.out.println(x + " - " + y + " = " + roznica);
+        System.out.println(x + " * " + y + " = " + iloczyn);
+        System.out.println(x + " / " + y + " = " + iloraz);
     }
 
     // 2) Stwórz tablicę liczb całkowitych a następnie oblicz średnią wartość.
@@ -48,6 +49,10 @@ class excercises {
         }
     }
 
+    public static double roundTo2DecimalPlace(double bmi) {
+        return Math.round(bmi * 100.0) / 100.0;
+    }
+
     // 4) Napisz program obliczający wskaźnik masy ciała (BMI).
     public static void exercise4() {
         double waga = 83;
@@ -55,7 +60,7 @@ class excercises {
 
         double bmi = waga / (wzrost * wzrost);
 
-        System.out.println("Twoje BMI wynosi: " + bmi);
+        System.out.println("Twoje BMI wynosi: " + roundTo2DecimalPlace(bmi));
 
         if (bmi < 18.5) {
             System.out.println("Masz niedowagę :(");
@@ -72,34 +77,55 @@ class excercises {
     // średnią ocen i określi odpowiedni stopień zaliczenia (np. "A", "B", "C"
     // itp.).
     public static void exercise5() {
+        int[] oceny = { 4, 5, 2, 5, 2 };
 
-        // A (>79% wiedzy), B (70–79%), C (60–69%), D (50–59%) i E (poniżej 49%)
-        int[] oceny = { 40, 81, 72, 54, 46, 38, 61 };
-
-        int suma = 0;
-        for (int liczba : oceny) {
-            suma += liczba;
+        double sumaOcen = 0;
+        for (int ocena : oceny) {
+            sumaOcen += ocena;
         }
 
-        double srednia = (double) suma / oceny.length;
-        System.out.println("Średnia: " + srednia);
+        double sredniaOcen = sumaOcen / oceny.length;
 
-        if (srednia < 49) {
-            System.out.println("Twoja ocena to E");
-        } else if (srednia < 50) {
-            System.out.println("Twoja ocena to D");
-        } else if (srednia < 60) {
-            System.out.println("Twoja ocena to C");
-        } else if (srednia < 70) {
-            System.out.println("Twoja ocena to B");
-        } else if (srednia < 79) {
-            System.out.println("Twoja ocena to A");
+        String stopienZaliczenia;
+        if (sredniaOcen >= 4.75) {
+            stopienZaliczenia = "A";
+        } else if (sredniaOcen >= 4.5) {
+            stopienZaliczenia = "A-";
+        } else if (sredniaOcen >= 4) {
+            stopienZaliczenia = "B+";
+        } else if (sredniaOcen >= 3.5) {
+            stopienZaliczenia = "B";
+        } else if (sredniaOcen >= 3) {
+            stopienZaliczenia = "B-";
+        } else if (sredniaOcen >= 2) {
+            stopienZaliczenia = "C";
+        } else {
+            stopienZaliczenia = "Nie zaliczono";
         }
+
+        System.out.println("Średnia ocen: " + sredniaOcen);
+        System.out.println("Stopień zaliczenia: " + stopienZaliczenia);
     }
 
     // 6) Napisz program, który sprawdza, czy dane słowo jest palindromem. Link:
     // https://pl.wikipedia.org/wiki/Palindrom
     public static void exercise6() {
+        String slowo = "kajak";
 
+        if (czyPalindrom(slowo)) {
+            System.out.println("Słowo \"" + slowo + "\" jest palindromem.");
+        } else {
+            System.out.println("Słowo \"" + slowo + "\" nie jest palindromem.");
+        }
+    }
+
+    public static boolean czyPalindrom(String slowo) {
+        int dlugosc = slowo.length();
+        for (int i = 0; i < dlugosc / 2; i++) {
+            if (slowo.charAt(i) != slowo.charAt(dlugosc - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
