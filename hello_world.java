e3acdf88f56fcbf2ae1b509295ad5389f350f77e
Quantess10 <lukaszolesik@gmail.com>
2024-03-02 09:18:43 +0100

    hello world in java

--- hello_world.java
+++ hello_world.java
@@ -1,8 +1,23 @@
 
-public class hello_world {
+class hello_world {
     public static void main(String[] args) {
-        String x = "Hello world in Java!";
+        String x = "Hello world in Java! :) :) :)";
+        String y;
+
+        final String Z = "Stała - ten tekst nie może być zmieniony.";
+
+        int integerValue = 5;
+        double doubleValue = 3.14;
+
+        y = "Tekst dla drugiej zmiennej";
+        y = "Zmieniona wartość y";
+
         System.out.println(x);
+        System.out.println(y);
+        System.out.println(Z);
+        System.out.println("---------");
+        System.out.println(integerValue);
+        System.out.println(doubleValue);
     }
 
 }
