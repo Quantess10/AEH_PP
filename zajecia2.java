5ac59961475e933f4641164f32828dc9f95c65bb
Quantess10 <lukaszolesik@gmail.com>
2024-03-03 08:59:06 +0100

    zajecia 03.03.24

--- zajecia2.java
+++ zajecia2.java
@@ -1,5 +1,10 @@
 public class zajecia2 {
     public static void main(String[] args) {
         System.out.println("test");
+
+        String text = "remote: Resolving deltas: 100% (1/1), completed with 1 local object.";
+        text = "@$SDA@$ADAERR#RASDF";
+
+        System.out.println(text);
     }
 }
